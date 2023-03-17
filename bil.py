c154ddfa00ce5542e171dee59859bfef307ba489
Anahita Amiri <anam0608@storsjogymnasiet.se>
2023-03-17 13:23:32 +0100

    bilar

--- bil.py
+++ bil.py
@@ -1,8 +1,23 @@
-#det här är en klass man kan skapa objekt av 
-class Bill:
+#Detta är en klass som man kan skapa egna Objekt av
+class Bil:
+    
 
-    #constructors körs först när man skaoar objekt av klasser   
-     def _init_(self, fabrikat, color, lager):
-         self.fabrikat = fabrikat
-         self.color = color
-         self.lager = lager 
+    #constructor körs först när klass objektet initieras
+    def __init__(self, fabrikat, color, lager):
+        self.fabrikat = fabrikat
+        self.color = color
+        self.lager = lager
+
+    #funktioner som tillhör en klass kallas för metoder=Metods"
+    def set_lager(self, lager):
+        self.lager = lager
+
+    def get_lager(self):
+        return self.lager
+    
+    def get_fabrikat(self):
+        return self.fabrikat
+    
+    def get_color(self):
+        return self.color
+    
